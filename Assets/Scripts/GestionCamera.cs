48d341b19402981155a894514f2812fa2cb06b79
Venec2 <virgile.jumin@brassart.fr>
2025-04-07 14:16:44 +0200

    camera

--- Assets/Scripts/GestionCamera.cs
+++ Assets/Scripts/GestionCamera.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GestionCamera : MonoBehaviour
@@ -8,7 +10,11 @@ public class GestionCamera : MonoBehaviour
     Transform m_perso1;
     [SerializeField]
     Transform m_perso2;
+    [SerializeField]
+    Camera cam;
     float posCameraX;
+    float posCameraY;
+    float size;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +24,15 @@ public class GestionCamera : MonoBehaviour
     void Update()
     {
     posCameraX = (m_perso1.position.x + m_perso2.position.x)/2;
-    gameObject.transform.position = new Vector3(posCameraX, gameObject.transform.position.y, gameObject.transform.position.z);
+    posCameraY = (m_perso1.position.y + m_perso2.position.y)/2;
+    gameObject.transform.position = new Vector3(posCameraX, posCameraY, gameObject.transform.position.z);
+
+    size = (Math.Abs(m_perso1.position.x - m_perso2.position.x)/5)+2;
+
+    cam.orthographicSize = size;
+
+
+    Debug.Log(m_perso1.position.x - m_perso2.position.x);
     
     }
 }
